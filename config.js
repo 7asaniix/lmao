3d06946e9e443aa82413b688d5af2e32cd79d923
Glitch (botforsale-z) <none>
2020-11-18 13:29:10 +0000

    🍋🌑 Updated with Glitch

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NzE3NjMxMDAzNjQzODcxMjYz.XtdH9A.zrY13QyQmKamF24mMgCUOVN34-c", //تعديل مهم توكن بوتك
+  YT_API_KEY: "",
+  prefix: "=",
+  devs: ["513058376092090393"] ///تعديل مهم غير هذا الرقم بـ ايدي حسابك
+};
